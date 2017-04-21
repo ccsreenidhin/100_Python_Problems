8dff7232699e1b25b9d5a947ccbbdd797b5eb173
c c sreenidhin <ccsreenidhin@gmail.com>
2017-04-21 19:14:00 +0530

    workshop

---
+++ Learning_Python_100_problems/python10.py
@@ -0,0 +1,26 @@
+###############################################################################################################
+###################################################SREENIDHIN C C##############################################
+###############################################################################################################
+
+"""Question 10
+Level 2
+
+Question:
+Write a program that accepts a sequence of whitespace separated words as input and prints the words after removing all duplicate words and sorting them alphanumerically.
+Suppose the following input is supplied to the program:
+hello world and practice makes perfect and hello world again
+Then, the output should be:
+again and hello makes perfect practice world
+
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
+"""
+
+
+words = raw_input("Enter the words: ")
+
+liwords = list(sorted(set(words.split(' '))))
+
+print ' '.join(liwords)
+
+
