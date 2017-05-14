698878e9614c2ef53ee9389ec5c1d1637fb7289d
c c sreenidhin <ccsreenidhin@gmail.com>
2017-05-15 00:59:00 +0530

    workshop

---
+++ Learning_Python_100_problems/python76.py
@@ -0,0 +1,13 @@
+###############################################################################################################
+###################################################SREENIDHIN C C##############################################
+###############################################################################################################
+
+"""
+Question:
+
+Please write a program to output a random number, which is divisible by 5 and 7, between 0 and 10 inclusive using random module and list comprehension.
+"""
+
+import random
+
+print random.choice([i for i in range(101) if i%5==0 and i%7==0])
