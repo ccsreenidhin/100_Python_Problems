2de361998f593beab635d282a4ceb74a8240a476
c c sreenidhin <ccsreenidhin@gmail.com>
2017-05-22 23:35:00 +0530

    workshop

---
+++ Learning_Python_100_problems/python44.py
@@ -0,0 +1,18 @@
+###############################################################################################################
+###################################################SREENIDHIN C C##############################################
+###############################################################################################################
+"""
+2.14
+
+Question:
+Write a program which accepts a string as input to print "Yes" if the string is "yes" or "YES" or "Yes", otherwise print "No". 
+"""
+
+ip = raw_input("Enter the string: ")
+
+if ip=='yes' or ip=='YES' or ip=='Yes':
+    print 'Yes'
+else:
+    print 'No'
+    
+
