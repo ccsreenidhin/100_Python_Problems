7c9453ddb7c7f893cc02f4c00c32d74c4172c2d8
c c sreenidhin <ccsreenidhin@gmail.com>
2017-06-02 21:48:00 +0530

    workshop

---
+++ Learning_Python_100_problems/python39.py
@@ -0,0 +1,19 @@
+###############################################################################################################
+###################################################SREENIDHIN C C##############################################
+###############################################################################################################
+"""
+2.10
+
+Question:
+Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included). Then the function needs to print the last 5 elements in the list.
+"""
+
+def listofno(a,b):
+    li =[]
+    for i in range(a,b+1):
+        li.append(i**2)
+    print li[-5:]
+        
+        
+        
+listofno(1,20)
---
+++ Learning_Python_100_problems/python4.py
@@ -0,0 +1,21 @@
+###############################################################################################################
+###################################################SREENIDHIN C C##############################################
+###############################################################################################################
+
+""" Question 4
+Level 1
+
+Question:
+Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
+Suppose the following input is supplied to the program:
+34,67,55,33,12,98
+Then, the output should be:
+['34', '67', '55', '33', '12', '98']
+('34', '67', '55', '33', '12', '98')
+"""
+
+x = raw_input("enter sequence")
+print x.split(',')
+print tuple(x.split(','))
+
+	
